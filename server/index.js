3d7876dfa3051df96752ed44f265a487f9f4f6f2
Justin Smid <justin250899@hotmail.com>
2020-08-11 20:09:37 +0200

    Generalize sending requests to LCU API from server

--- server/index.js
+++ server/index.js
@@ -2,88 +2,66 @@ const express = require('express');
 const fetch = require('node-fetch');
 const LCUConnector = require('lcu-connector');
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-
 Buffer = Buffer || require('buffer').Buffer;
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const lcuConnector = new LCUConnector();
 
-const app = express();
-const port = 3000;
-
 const btoa = string => Buffer.from(string).toString('base64');
 
-const get = endpoint => {
-    const url = `https://127.0.0.1:${global.LCU_data.port}${endpoint}`;
+const app = express();
+const PORT = 3000;
 
-    return fetch(url, {
+app.get('/request', async (req, res) => {
+    handleRequest(req, res, 'GET');
+});
+
+app.post('/request', (req, res) => {
+    handleRequest(req, res, 'POST');
+});
+
+const handleRequest = async (req, res, type = 'GET') => {
+    const {endpoint} = req.query;
+    console.log(`Handling [${type}] request for endpoint '${endpoint}'...`);
+
+    const response = await sendRequest(endpoint, {method: type});
+
+    console.log(`Response for endpoint '${endpoint}': `, response);
+
+    res.json(response);
+};
+
+const sendRequest = (endpoint, options = {}) => {
+    const {protocol, address, port} = global.LCU_data;
+    const url = `${protocol}://${address}:${port}${endpoint}`;
+
+    options = {
+        ...options,
         headers: {
             Accept: 'application/json',
-            Authorization: `Basic ${auth}`
+            Authorization: `Basic ${global.LCU_auth}`
         }
-    })
-        .then(res => res.json())
-        .then(res => {
-            console.log(`Request finished`, res);
-            return res;
-        })
-        .catch(err => console.log(`ERROR: `, err));
-}
+    };
 
-const post = endpoint => {
-    const url = `https://127.0.0.1:${global.LCU_data.port}${endpoint}`;
-
-    return fetch(url, {
-        headers: {
-            Accept: 'application/json',
-            Authorization: `Basic ${auth}`
-        },
-        method: 'POST'
-    })
+    return fetch(url, options)
         .then(res => res.json())
-        .then(res => {
-            console.log(`Request finished`, res);
-            return res;
-        })
         .catch(err => console.log(`ERROR: `, err));
 };
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/get', async (req, res) => {
-    const { endpoint } = req.query;
-    
-    const response = await get(endpoint);
-    console.log('GET response', response);
-
-    res.json(response);
-});
-
-app.post('/get', async (req, res) => {
-    const { endpoint } = req.query;
-    
-    const response = await post(endpoint);
-    console.log('POST response', response);
-
-    res.json(response);
-});
-
-app.listen(port, () => {
+app.listen(PORT, () => {
     lcuConnector.on('connect', data => {
         console.log('LCU data: ', data);
-        
+
         global.LCU_data = data;
-        global.LCU_username = 'riot';
-        global.LCU_auth = `${btoa(`${global.LCU_username}:${global.LCU_data.password}`)}`;
+        global.LCU_auth = `${btoa(`${global.LCU_data.username}:${global.LCU_data.password}`)}`;
     });
-!
+    
     lcuConnector.on('disconnect', () => {
         console.warn(`WARN: LCU connector disconnected.`);
     });
 
     lcuConnector.start();
-    
-    console.log(`Example app listening at http://localhost:${port}`);
+
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
