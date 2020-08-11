8d199434f65926f2dea2a60ba60d29fc11c65206
Justin Smid <justin250899@hotmail.com>
2020-08-11 19:55:23 +0200

    Un-hardcode LCU data from lockfile

--- server/index.js
+++ server/index.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const LCUConnector = require('lcu-connector');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 Buffer = Buffer || require('buffer').Buffer;
 
-const LOCKFILE_VALUE = 'LeagueClient:17840:58489:z_3kZYgsM0JtqYsOYvfgHg:https';
-const LCU_username = 'riot';
-const [LCU_process, LCU_PID, LCU_port, LCU_password, LCU_protocol] = LOCKFILE_VALUE.split(':');
+const lcuConnector = new LCUConnector();
 
 const app = express();
 const port = 3000;
 
 const btoa = string => Buffer.from(string).toString('base64');
 
-const auth = `${btoa(`${LCU_username}:${LCU_password}`)}`;
-
 const get = endpoint => {
-    const url = `https://127.0.0.1:${LCU_port}${endpoint}`;
+    const url = `https://127.0.0.1:${global.LCU_data.port}${endpoint}`;
 
     return fetch(url, {
         headers: {
@@ -34,7 +31,7 @@ const get = endpoint => {
 }
 
 const post = endpoint => {
-    const url = `https://127.0.0.1:${LCU_port}${endpoint}`;
+    const url = `https://127.0.0.1:${global.LCU_data.port}${endpoint}`;
 
     return fetch(url, {
         headers: {
@@ -74,5 +71,19 @@ app.post('/get', async (req, res) => {
 });
 
 app.listen(port, () => {
+    lcuConnector.on('connect', data => {
+        console.log('LCU data: ', data);
+        
+        global.LCU_data = data;
+        global.LCU_username = 'riot';
+        global.LCU_auth = `${btoa(`${global.LCU_username}:${global.LCU_data.password}`)}`;
+    });
+!
+    lcuConnector.on('disconnect', () => {
+        console.warn(`WARN: LCU connector disconnected.`);
+    });
+
+    lcuConnector.start();
+    
     console.log(`Example app listening at http://localhost:${port}`);
 });
