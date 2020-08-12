c9705710824581e430947795242151ce4192faec
Justin Smid <justin250899@hotmail.com>
2020-08-12 14:56:59 +0200

    Initial electron app.
    Has the starter page from create-react-app, aswell as our express server running in the background.

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ App to accept league queues using your phone.
 ### Download and install
 1. Install [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) (If not already installed, check if `git --version` works)
 2. Install [node](https://nodejs.org/en/download/) (If not already installed, check if `node --version` works)
-3. Install [expo](https://expo.io/) (Run `npm install -g expo-cli` to install it)
-4. Clone repository (`git clone https://github.com/justinsmid/accept-league-queue.git`)
+3. Install [expo](https://expo.io/)
+   - Run `npm install -g expo-cli` to install it
+4. Install [node-foreman](https://github.com/strongloop/node-foreman)
+   - Run `npm install -g foreman` to install it
+5. Clone repository (`git clone https://github.com/justinsmid/accept-league-queue.git`)
 
 ### Edit (currently) hardcoded things
 
---
+++ desktop/express-server/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const LCUConnector = require('lcu-connector');
+const ngrok = require('ngrok');
+
+Buffer = Buffer || require('buffer').Buffer;
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+const btoa = string => Buffer.from(string).toString('base64');
+
+let lcuConnector;
+
+const PORT = 6969;
+let server;
+
+module.exports.startExpressServer = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            lcuConnector = new LCUConnector();
+            server = express();
+            global.expressServerPort = PORT;
+
+            server.get('/', (req, res) => {
+                res.send('Hello, world!');
+            });
+
+            server.get('/request', async (req, res) => {
+                handleRequest(req, res, 'GET');
+            });
+
+            server.post('/request', (req, res) => {
+                handleRequest(req, res, 'POST');
+            });
+
+            server.listen(PORT, async () => {
+                global.ngrokUrl = await ngrok.connect(PORT);
+
+                lcuConnector.on('connect', data => {
+                    console.log('LCU data: ', data);
+
+                    global.LCU_data = data;
+                    global.LCU_auth = `${btoa(`${global.LCU_data.username}:${global.LCU_data.password}`)}`;
+
+                    resolve(true);
+                });
+
+                lcuConnector.on('disconnect', () => {
+                    console.warn(`WARN: LCU connector disconnected.`);
+                });
+
+                lcuConnector.start();
+
+                setTimeout(() => {
+                    reject(`Express server timed out while attempting to read LCU data. Probably because no running League Client was found.`);   
+                }, 10000);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+const handleRequest = async (req, res, type = 'GET') => {
+    const {endpoint} = req.query;
+    console.log(`Handling [${type}] request for endpoint '${endpoint}'...`);
+
+    const response = await sendRequest(endpoint, {method: type});
+
+    console.log(`Response for endpoint '${endpoint}': `, response);
+
+    res.json(response);
+};
+
+const sendRequest = (endpoint, options = {}) => {
+    const {protocol, address, port} = global.LCU_data;
+    const url = `${protocol}://${address}:${port}${endpoint}`;
+
+    options = {
+        ...options,
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${global.LCU_auth}`
+        }
+    };
+
+    return fetch(url, options)
+        .then(res => res.json())
+        .catch(err => console.log(`ERROR: `, err));
+};
